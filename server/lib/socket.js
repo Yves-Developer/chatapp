db2380bd8c7a6941626440d239a1e1e2b90b5905
Yves-Developer <yvesmugisha09@gmail.com>
2025-02-07 18:14:35 +0200

    ## Allowing Coonect ion To backend URL

--- server/lib/socket.js
+++ server/lib/socket.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["https://chatify-zhfb.onrender.com/"], // Make sure this is your frontend URL
+    origin: ["http://localhost:5173"], // Make sure this is your frontend URL
     credentials: true,
   },
 });
