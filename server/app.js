c41fdc9e094bb698595b93cd5c0b62c664bdc0e0
Yves-Developer <yvesmugisha09@gmail.com>
2025-02-07 17:58:17 +0200

    ## Additional CORS config

--- server/app.js
+++ server/app.js
@@ -12,7 +12,9 @@ dotenv.config();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(
+  cors({ origin: "https://chatify-zhfb.onrender.com/", credentials: true })
+);
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
